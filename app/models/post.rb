46e8159bb289e428909e7b3cd4b3f8613ee078e6
8bit-m8 <you@example.com>
2024-07-09 16:16:49 +0300

    Add slug to posts.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,16 @@
 class Post < ApplicationRecord
   extend Mobility
+  extend FriendlyId
+
   translates :body, backend: :action_text
   translates :title
 
+  friendly_id :title_en, use: :slugged
+
   validates :title_en, presence: true
   validates :title_ru, presence: true
+
+  def should_generate_new_friendly_id?
+    title_en_changed? || slug.blank?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_09_201624) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_09_201625) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -182,6 +182,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_09_201624) do
     t.datetime "updated_at", null: false
     t.string "title_en"
     t.string "title_ru"
+    t.string "slug"
+    t.index ["slug"], name: "index_posts_on_slug", unique: true
     t.index ["title_en"], name: "index_posts_on_title_en"
     t.index ["title_ru"], name: "index_posts_on_title_ru"
   end
